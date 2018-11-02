4e9a28a2d015bfc6bc21cbf2684782ef6ca80392
josh <critesjosh@gmail.com>
2018-11-02 14:17:09 -0400

    first commit

---
+++ contracts/Coin.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.21;
+
+contract Coin {
+    // The keyword "public" makes those variables
+    // readable from outside.
+    address public minter;
+    mapping (address => uint) public balances;
+
+    // Events allow light clients to react on
+    // changes efficiently.
+    event Sent(address from, address to, uint amount);
+
+    // This is the constructor whose code is
+    // run only when the contract is created.
+    constructor() public {
+        minter = msg.sender;
+    }
+
+    function mint(address receiver, uint amount) public {
+        require(msg.sender == minter, "Must be minter.");
+        balances[receiver] += amount;
+    }
+
+    function send(address receiver, uint amount) public {
+      // require(balances[msg.sender] >= amount, "Insufficient funds");
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit Sent(msg.sender, receiver, amount);
+    }
+}
